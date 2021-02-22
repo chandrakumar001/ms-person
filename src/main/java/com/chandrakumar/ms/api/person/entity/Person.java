78be11bcb1da2744f19623d8f93ce33b4ba857d6
Chandra Kumar <osaimar19@gmail.com>
2021-02-22 11:45:35 +0530

    updated audit model for person api

--- src/main/java/com/chandrakumar/ms/api/person/entity/Person.java
+++ src/main/java/com/chandrakumar/ms/api/person/entity/Person.java
@@ -1,16 +1,11 @@
 package com.chandrakumar.ms.api.person.entity;
 
 import com.chandrakumar.ms.api.common.audit.Action;
+import com.chandrakumar.ms.api.common.audit.Auditable;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Date;
 import java.util.UUID;
 
 import static javax.persistence.EnumType.STRING;
@@ -18,8 +13,7 @@ import static javax.persistence.EnumType.STRING;
 @Entity
 @Table(name = "person", schema = "person")
 @Data
-@EntityListeners(AuditingEntityListener.class)
-public class Person implements Serializable {
+public class Person extends Auditable implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -30,25 +24,6 @@ public class Person implements Serializable {
     private String age;
     private String favouriteColour;
 
-    @CreatedBy
-    protected String createdBy;
-
-    @CreatedDate
-    @Temporal(TemporalType.TIMESTAMP)
-    protected Date creationDate;
-
-    @LastModifiedBy
-    protected String lastModifiedBy;
-
-    @LastModifiedDate
-    @Temporal(TemporalType.TIMESTAMP)
-    protected Date lastModifiedDate;
-
     @Enumerated(STRING)
     private Action action;
-
-    @PreRemove
-    public void preRemove() {
-        this.setAction(Action.DELETED);
-    }
 }
