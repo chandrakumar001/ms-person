21a6a98b595d28cd96689084e0c6f4c1a6d6a57c
Chandra Kumar <osaimar19@gmail.com>
2021-05-14 12:35:15 +0530

    Updated service portnumber

---
+++ k8s/kubectl-cleanup.bat
@@ -0,0 +1,7 @@
+kubectl delete deployment ms-person
+kubectl delete service ms-person
+kubectl delete ingress ms-person
+kubectl delete networkPolicy ms-person
+kubectl delete hpa ms-person
+kubectl delete vpa ms-person
+pause
