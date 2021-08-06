d1834a128a520b32b0656dc24eb5190d9930cf65
Chandra Kumar <osaimar19@gmail.com>
2021-08-06 09:54:19 +0530

    code coverage plugin

--- src/test/groovy/com/chandrakumar/ms/api/person/resource/DefaultPersonCommandControllerSpec.groovy
+++ src/test/groovy/com/chandrakumar/ms/api/person/resource/DefaultPersonCommandControllerSpec.groovy
@@ -2,15 +2,21 @@ package com.chandrakumar.ms.api.person.resource
 
 import com.chandrakumar.ms.api.person.service.PersonCommandService
 import com.chandrakumar.ms.api.person.service.PersonMockData
+import com.chandrakumar.ms.api.person.swagger.model.PersonBareDTO
 import com.chandrakumar.ms.api.person.swagger.model.PersonDTO
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import static com.chandrakumar.ms.api.person.service.PersonMockData.personBareDTO
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 
 @Unroll
 // No @ContextConfiguration with Spring MVC Test!
@@ -19,7 +25,7 @@ class DefaultPersonCommandControllerSpec extends Specification {
     // Don't want to call real service, so use mock
     def personCommandService = Mock(PersonCommandService)
     ObjectMapper objectMapper;
-    def mockMvc = null;
+    MockMvc mockMvc = null;
 
     /**
      * Runs before each test method, like the JUnit Before
@@ -36,7 +42,7 @@ class DefaultPersonCommandControllerSpec extends Specification {
                 .build()
     }
 
-    def "should delete personDTO object"() {
+    def "should delete person object"() {
         given:
 
         personCommandService.deletePerson(_ as String) >> null
@@ -50,5 +56,80 @@ class DefaultPersonCommandControllerSpec extends Specification {
 
         then:
         1 * personCommandService.deletePerson(_ as String) >> null
+    }
+
+    def "should create person object"() {
+        given:
+        def emailId = "osaimar19@gmail.com"
+        def firstName = "chandra"
+        def lastName = "kumar"
+        def age = "28"
+
+        final PersonBareDTO personBareDTO = personBareDTO(emailId, firstName, lastName, age)
+        personCommandService.createPerson(_ as PersonBareDTO) >> PersonMockData.personDTO(
+                emailId,
+                firstName,
+                lastName,
+                age
+        )
+        when:
+        // Try different URL or Method to see what will happen!
+        MockHttpServletRequestBuilder builder = post("/v1/people")
+                .content(asJsonString(personBareDTO))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+
+        MvcResult mvcResult = mockMvc.perform(builder)
+                .andReturn()
+
+        then:
+        1 * personCommandService.createPerson(_ as PersonBareDTO) >> PersonMockData.personDTO(
+                emailId,
+                firstName,
+                lastName,
+                age
+        )
+    }
+
+    def "should update person object"() {
+        given:
+        def personId = "d6f02a17-c676-4b1b-ae39-e3b12f47c407"
+        def emailId = "osaimar19@gmail.com"
+        def firstName = "chandra"
+        def lastName = "kumar"
+        def age = "28"
+
+        final PersonBareDTO personBareDTO = personBareDTO(emailId, firstName, lastName, age)
+        personCommandService.updatePerson(_ as String, _ as PersonBareDTO) >> PersonMockData.personDTO(
+                emailId,
+                firstName,
+                lastName,
+                age
+        )
+        when:
+        // Try different URL or Method to see what will happen!
+        MockHttpServletRequestBuilder builder = put("/v1/people/" + personId)
+                .content(asJsonString(personBareDTO))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+
+        MvcResult mvcResult = mockMvc.perform(builder)
+                .andReturn()
+
+        then:
+        1 * personCommandService.updatePerson(_ as String, _ as PersonBareDTO) >> PersonMockData.personDTO(
+                emailId,
+                firstName,
+                lastName,
+                age
+        )
+    }
+
+    static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
